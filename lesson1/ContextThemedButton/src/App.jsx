c4ad2e363837db317442f0d8275127dd42fdbb96
Nazar <panchenko.nazar.a@gmail.com>
2020-11-21 15:23:04 +0200

    done

---
+++ lesson1/ContextThemedButton/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './index.scss';
+import ThemedButton from './ThemedButton';
+import { themes } from './themes-context';
+
+class App extends React.Component {
+    state = {
+        theme: themes.light
+    };
+
+    toggleTheme = () => {
+        const newTheme = 
+            this.state.theme === themes.dark ? themes.light : themes.dark;
+
+        this.setState({
+            theme: newTheme
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <ThemeContext.Provider value={this.state.theme}>     
+                    <ThemedButton onClick={this.toggleTheme}>
+                        Dynamic Theme
+                    </ThemedButton>
+                </ThemeContext.Provider>
+                <ThemedButton onClick={this.toggleTheme}>
+                    Default Theme
+                </ThemedButton>
+            </>
+        );
+    }
+}
+
+export default App;
