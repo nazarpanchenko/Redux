3dcf2c19785e3b4cd1b7bd1b508be1128892fd34
Nazar <panchenko.nazar.a@gmail.com>
2020-11-22 16:42:10 +0200

    Edit

--- lesson2/UsersStore/src/users.reducer.js
+++ lesson2/UsersStore/src/users.reducer.js
@@ -3,9 +3,13 @@ import { ADD, DELETE } from './users.actions';
 export const userReducer = (state = { usersList: [] }, action) => {
     switch(action.type) {
         case ADD:
-            return state.usersList.concat(action.user);
+            return {
+                usersList: state.usersList.concat(action.user)
+            };
         case DELETE:
-            return state.usersList.splice(action.userId, 1);
+            return {
+                usersList: state.usersList.splice(action.userId, 1)
+            };
         default :
             return state;
     }
