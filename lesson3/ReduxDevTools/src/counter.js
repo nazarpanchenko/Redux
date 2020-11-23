45098d7d16c088ca49eb011bfa6d6d906042d422
Nazar <panchenko.nazar.a@gmail.com>
2020-11-23 14:05:50 +0200

    Edit

---
+++ lesson3/ReduxDevTools/src/counter.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'COUNTER/INCREMENT':
+            return state + 1;
+        case 'COUNTER/DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const counterReducer = createStore(counterReducer);
+
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
