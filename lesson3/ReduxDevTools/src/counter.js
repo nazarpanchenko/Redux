05784dee6195a83fa0ccef75d4515263b3d1099e
Nazar <panchenko.nazar.a@gmail.com>
2020-11-23 14:09:08 +0200

    Edit

--- lesson3/ReduxDevTools/src/counter.js
+++ lesson3/ReduxDevTools/src/counter.js
@@ -3,6 +3,18 @@ import { createStore } from "redux";
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 
+const increment = () => {
+    return {
+        type: INCREMENT
+    };
+};
+
+const decrement = () => {
+    return {
+        type: DECREMENT
+    };
+};
+
 const counterReducer = (state = 0, action) => {
     switch (action.type) {
         case INCREMENT:
