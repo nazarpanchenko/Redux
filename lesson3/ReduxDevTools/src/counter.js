5756d58c433a993877c01f19b3b3c5bdf41989d3
Nazar <panchenko.nazar.a@gmail.com>
2020-11-23 14:07:36 +0200

    Edit

--- lesson3/ReduxDevTools/src/counter.js
+++ lesson3/ReduxDevTools/src/counter.js
@@ -1,10 +1,13 @@
 import { createStore } from "redux";
 
-export const counterReducer = (state = 0, action) => {
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+const counterReducer = (state = 0, action) => {
     switch (action.type) {
-        case 'COUNTER/INCREMENT':
+        case INCREMENT:
             return state + 1;
-        case 'COUNTER/DECREMENT':
+        case DECREMENT:
             return state - 1;
         default:
             return state;
