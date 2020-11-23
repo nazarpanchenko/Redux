ca9075a9a378bae6657cb1b68231f3876048427e
Nazar <panchenko.nazar.a@gmail.com>
2020-11-23 17:17:00 +0200

    Edit

--- lesson3/Store/src/cart.reducer.js
+++ lesson3/Store/src/cart.reducer.js
@@ -5,23 +5,25 @@ import { ADD_CART, DELETE_CART } from './cart.actions';
 //         products: []
 //     }
 // };
-const cart = {
-    products: []
+const initialValue = {
+    cart: {
+        products: []
+    }
 };
 
-const cartReducer = (state = cart, action) => {
+const cartReducer = (state = initialValue, action) => {
     switch (action.type) {
         case ADD_CART:
             return {
                 ...state,
                 cart: {
                     ...state.cart,
-                    products: state.products.concat(action.payload.product)
+                    products: state.cart.products.concat(action.payload.product)
                 }
             };
 
         case DELETE_CART:
-            const updatedProducts = state.products.map(product => {
+            const updatedProducts = state.cart.products.map(product => {
                 if (product.id !== action.payload.productId) {
                     return {
                         ...state,
