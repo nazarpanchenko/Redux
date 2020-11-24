010384c30750b979975a93d482b78e3dbd6e6c1e
Nazar <panchenko.nazar.a@gmail.com>
2020-11-24 16:58:19 +0200

    done

--- lesson4/Counter/src/App.jsx
+++ lesson4/Counter/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Counter from './components/Counter';
+import Counter from './Counter';
 import store from './store';
 
 export const App = () => {
