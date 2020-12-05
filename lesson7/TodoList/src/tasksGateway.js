6f615e0bd36098ee43be9720a21dcdc0253a3715
Nazar <panchenko.nazar.a@gmail.com>
2020-12-05 17:46:51 +0200

    initial commit

---
+++ lesson7/TodoList/src/tasksGateway.js
@@ -0,0 +1,54 @@
+const baseUrl = 'https://5f4107e4a5e9db0016302376.mockapi.io/api/v1/tasks'
+
+export const createTask = (taskData) => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;utc-8',
+        },
+        body: JSON.stringify(taskData),
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Failed to create task')
+        }
+    })
+}
+
+export const fetchTasksList = () => {
+    return fetch(baseUrl)
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            }
+        })
+        .then((tasksList) =>
+            tasksList.map(({ _id, ...task }) => ({
+                id: _id,
+                ...task,
+            }))
+        )
+}
+
+export const updateTask = (taskId, taskData) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json;utc-8',
+        },
+        body: JSON.stringify(taskData),
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Failed to update task')
+        }
+    })
+}
+
+export const deleteTask = (taskId) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'DELETE',
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Failed to delete task')
+        }
+    })
+}
